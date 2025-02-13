0e7282dc9400830a414cbe56b94af06a3a838f01
verdantgreeny <verdantgreeny@gmail.com>
2025-02-13 09:46:01 +0900

    25-02-13

--- part2/02.배열/(lv.3)나이-통계-구하기.js
+++ part2/02.배열/(lv.3)나이-통계-구하기.js
@@ -10,7 +10,26 @@
  * @returns {{ averageAge: number, maxAge: number }}
  */
 
-function calculateStatistics(users) {}
+function calculateStatistics(users) {
+  //빈 배열 전달할떄
+  if (users.length === 0) {
+    return { averageAge: 0, maxAge: 0 };
+  }
+  //평균 나이(averageAge
+  const totalAge = users.reduce((acc, curr) => {
+    return acc + curr.age;
+  }, 0);
+
+  const averageAge = totalAge / users.length;
+
+  // 최대 나이(maxAge)
+  const maxAge = users.reduce((acc, curr) => {
+    return acc > curr.age ? acc : curr.age;
+  }, users[0].age);
+
+  // { averageAge, maxAge } 객체 형태로 반환해야 합니다.
+  return { averageAge, maxAge };
+}
 
 // export 를 수정하지 마세요.
 export { calculateStatistics };
