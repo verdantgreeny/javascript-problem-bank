18f143b355c7dc9220fd5d5193788556e8594876
verdantgreeny <verdantgreeny@gmail.com>
2025-02-26 09:53:33 +0900

    25-02-26

--- part2/03.비동기/(lv.3)timeout.js
+++ part2/03.비동기/(lv.3)timeout.js
@@ -12,7 +12,15 @@
  */
 
 // TODO: timeOut 함수를 작성해주세요.
-async function timeOut(promise, ms) {}
+async function timeOut(promise, ms) {
+  const rejectPromise = new Promise((_, reject) =>
+    setTimeout(() => reject("timeout"), ms)
+  );
+  // 더 먼저 끝난 걸 먼저 반환(인자 둘을 경쟁 시킴)
+  // -> 즉 promise가 시간 안에 끝나면 promise 반환하고,
+  // 그렇지 않으면   "timeout"을 reject하는 Promise를 반환
+  return await Promise.race([promise, rejectPromise]);
+}
 
 // export 를 수정하지 마세요.
 export { timeOut };
